1e70d79240716f6a39062646e053a5bf3d2cc420
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-11-04 00:10:16 -0400

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -29,4 +29,5 @@ Some Python projects to try out:
 st.markdown(text)
 
 st.sidebar.image("https://avatars.githubusercontent.com/u/59750436?v=4")
-st.sidebar.write("Bogdan delivers a variety of courses at The Marquee Group that focus on financial modeling, data sciences, and programming."
+st.sidebar.write("Bogdan delivers a variety of courses at The Marquee Group that focus on financial modeling, data sciences, and programming.")
+st.sidebar.write("Medium: https://medium.com/@bogdan-tudose")
