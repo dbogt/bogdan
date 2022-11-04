94410e9eb186d24e1e577e5a1ce5912c8b989ed5
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-11-04 00:01:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,46 @@
 import streamlit as st
 
-st.markdown("# Main page 🎈")
+st.markdown("# Welcome 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
+text = """
+### Hi there 👋
+
+![](https://komarev.com/ghpvc/?username=your-github-username&color=blue)
+
+- :computer: Connect with me on **[LinkedIn](https://www.linkedin.com/in/tudosebogdan/)**
+- :newspaper: Read some of my Python articles on **[Medium](https://medium.com/@bogdan-tudose)**
+- :office: I'm currently a Principal and instructor for **[The Marque Group](https://marqueegroup.ca/)**
+- :school: Check out some of the programming courses I teach:
+  - [Python 1: Core Data Analysis](https://marqueegroup.ca/course/python-1-core-data-analysis/)  
+  - [Python 2: Visualization and Analysis](https://marqueegroup.ca/course/python-2-visualization-and-analysis/)
+  - [Python 3: Web Scraping and Machine Learning](https://marqueegroup.ca/course/python-3-web-scraping-and-machine-learning/)
+  - [VBA for Finance Professionals](https://marqueegroup.ca/course/vba-for-finance-professionals/)
+- Python Resources: https://github.com/dbogt/pythonResources
+
+Some Python projects to try out:
+- :fuelpump:[Gas Stations Locator](https://bit.ly/locationsDemo): Dashboard created with Streamlit and Python to plot gas station locations of Couche-Tard and Shell Canada. Pick a gas station and km radius and the app will locate all the Couche-Tard and Shell stations within that distance. App is a demo of how Python could be used in an M&A deal to help with cost synergy analysis. [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](hhttps://dbogt-locationsdemo-app-avfon4.streamlitapp.com/) 
+- :chart_with_upwards_trend:[Stock Beta Calculator](https://bitly.com/StockBetaApp): Dashboard created with Streamlit and Python to calculate a stock's beta. Pick a ticker and a stock index to run the regression and calculate the beta. [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/dbogt/stockbetadashboard/main/app.py) 
+- :chart_with_upwards_trend:[Options Calculator](https://bit.ly/OptionsCalculator): Dashboard created with Streamlit and Python to calculate a call/put option value with Black Scholes. App also allows to scrape options chains from Yahoo Finance for a particular ticker and visualizes strike prices vs. implied vols and bid/ask/last price of option. [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/dbogt/optionscalculator/main/app.py) 
+- :moneybag:[Ontario Sunshine List](https://bit.ly/ONSunshineList): Dashboard that explores 20+ years of public sector salary disclosures from Ontario (https://www.ontario.ca/page/public-sector-salary-disclosure). [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/dbogt/on_sunshine/main) 
+- :movie_camera::trophy:[Oscars 2022 Predictions](https://bitly.com/oscarsApp): Make predictions for the top categories and compare your answers with other people around the world. Also try out the Best Picture Emoji quiz while you're at it! [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/dbogt/oscars2022/main) 
+- :game_die:[Board Game Collection](https://bitly.com/BGGApp): Check out your board game collection by connecting with your BoardGameGeek account. [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/dbogt/bggcollection/main/app.py) 
+
+
+<!--
+**dbogt/dbogt** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+
+Here are some ideas to get you started:
+
+- 🔭 I’m currently working on ...
+- 🌱 I’m currently learning ...
+- 👯 I’m looking to collaborate on ...
+- 🤔 I’m looking for help with ...
+- 💬 Ask me about ...
+- 📫 How to reach me: ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+-->
+"""
+
+st.markdown(text)
