67a44320714d0c5282fd60a9c9154283fd69d710
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2023-01-19 11:52:21 -0500

    test

--- pages/5_Stock Beta Calculator.py
+++ pages/5_Stock Beta Calculator.py
@@ -238,7 +238,7 @@ dayStart = '{:%Y-%m-%d}'.format(startDate)
 dayEnd = '{:%Y-%m-%d}'.format(endDate)
 
 st.write(interval, dayStart,dayEnd)
-enel = yf.download("ENEL.MI",interval=interval,start=start,end=end)
+enel = yf.download("ENEL.MI",interval=interval,start=dayStart,end=dayEnd)
 st.write(enel)
 stockDF = grabPricingAll(stockDrop, interval, dayStart, dayEnd)
 indexDF = grabPricingAll(indexTicker, interval, dayStart, dayEnd)
