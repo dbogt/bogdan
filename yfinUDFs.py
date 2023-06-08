73ad8767292f742c453468f2670dd8aff158b189
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2023-06-08 12:57:18 -0400

    Update yfinUDFs.py
    updated with crumb and cookies

--- yfinUDFs.py
+++ yfinUDFs.py
@@ -1,86 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 15 23:19:32 2022
-@author: Bogdan Tudose
-"""
-#%% Yahoo Finance Functions
-#Source: modified from https://maikros.github.io/yahoo-finance-python/
-import requests                  # [handles the http interactions](http://docs.python-requests.org/en/master/) 
-from bs4 import BeautifulSoup    # beautiful soup handles the html to text conversion and more
-import re                        # regular expressions are necessary for finding the crumb (more on crumbs later)
-from datetime import datetime    # string to datetime object conversion
-from time import mktime          # mktime transforms datetime objects to unix timestamps
+from urllib.request import Request, urlopen  
 import pandas as pd
-from urllib.request import urlopen  
-from io import StringIO
 import json
+import requests
+import streamlit as st
 
-def get_crumbs_and_cookies(stock):
-    """
-    get crumb and cookies for historical data csv download from yahoo finance
-    parameters: stock - short-handle identifier of the company 
-    returns a tuple of header, crumb and cookie
-    """
-    
-    url = 'https://finance.yahoo.com/quote/{}/history'.format(stock)
-    with requests.session():
-        header = {'Connection': 'keep-alive',
-                   'Expires': '-1',
-                   'Upgrade-Insecure-Requests': '1',
-                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
-                   AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
-                   }
-        
-        website = requests.get(url, headers=header)
-        # soup = BeautifulSoup(website.text, 'lxml')
-        soup = BeautifulSoup(website.text)
-        crumb = re.findall('"CrumbStore":{"crumb":"(.+?)"}', str(soup))
+cookies = st.secrets['cookies']
 
-        return (header, crumb[0], website.cookies)
-    
-def convert_to_unix(date):
-    """
-    converts date to unix timestamp
-    parameters: date - in format (dd-mm-yyyy)
-    returns integer unix timestamp
-    """
-    datum = datetime.strptime(date, '%d-%m-%Y')
-    
-    return int(mktime(datum.timetuple())) + 86400 #adding 1 day due to timezone issue
+headers = {
+    'authority': 'query2.finance.yahoo.com',
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'origin': 'https://finance.yahoo.com',
+    'referer': 'https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch',
+    'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+}
 
-
-def fnYFinHist(stock, interval='1d', day_begin='01-01-2013', day_end='17-11-2021'):
-    """
-    queries yahoo finance api to receive historical data in csv file format
-    
-    parameters: 
-        stock - short-handle identifier of the company
-        interval - 1d, 1wk, 1mo - daily, weekly monthly data
-        day_begin - starting date for the historical data (format: dd-mm-yyyy)
-        day_end - final date of the data (format: dd-mm-yyyy)
-    
-    returns a list of comma seperated value lines
-    """
-    #stock = 'AAPL'
-    
-    day_begin_unix = convert_to_unix(day_begin)
-    day_end_unix = convert_to_unix(day_end)
-    header, crumb, cookies = get_crumbs_and_cookies(stock)
-    
-    with requests.session():
-        url = 'https://query1.finance.yahoo.com/v7/finance/download/' \
-              '{stock}?period1={day_begin}&period2={day_end}&interval={interval}&events=history&crumb={crumb}' \
-              .format(stock=stock, 
-                      day_begin=day_begin_unix, day_end=day_end_unix,
-                      interval=interval, crumb=crumb)
-                
-        website = requests.get(url, headers=header, cookies=cookies)
-
-    data = pd.read_csv(StringIO(website.text), parse_dates=['Date'], index_col=['Date'])
-    data['Returns'] = data['Close'].pct_change()
-    return data
-
+params = {
+    'crumb': st.secrets['crumb'],
+    'lang': 'en-US',
+    'region': 'US',
+    'corsDomain': 'finance.yahoo.com',
+}
 def fnYFinJSON(stock, field):
+    df = fnYFinJSONAll(stock)
+    if field in df.columns:
+        return df.iloc[0][field]
+    else:
+        return "N/A"
+    
+    
+
+
+def fnYFinJSONOLD(stock, field):
     if not stock:
         return "enter a ticker"
     else:
@@ -101,51 +58,25 @@ def fnYFinJSON(stock, field):
     else:
         return "N/A"
 
-def fnYFinJSONAll(stock):
-	urlData = "https://query2.finance.yahoo.com/v6/finance/quote?symbols="+stock
-	df = pd.read_json(urlData)
-	df = pd.DataFrame(df.iloc[1][0])#need to go down to result layer
-	df.set_index('symbol', inplace=True) #renaming row as the ticker to keep track of data
-	return df
+def fnYFinJSONAll(stock): 
+    urlData = "https://query2.finance.yahoo.com/v7/finance/quote?symbols="+stock 
+    response = requests.get(urlData, params=params, cookies=cookies, headers=headers)
+    #st.write("Running", stock)
+    try:
+        data = response.json()
+        df = pd.DataFrame(data['quoteResponse']['result'])
 
-#%% Option chains
-def grabExpDates(ticker):
-    """
-    Returns a dataframe of expiration dates available on Yahoo Finance.
-    e.g.: https://query2.finance.yahoo.com/v7/finance/options/SPY
-    
-    """
-    url = "https://query2.finance.yahoo.com/v7/finance/options/" + ticker
-    data = pd.read_json(url)
-    expDatesUnix = data['optionChain']['result'][0]['expirationDates']
-    expDatesNormal = pd.to_datetime(expDatesUnix, origin="unix",unit='s')
-    df = pd.DataFrame(expDatesUnix,index=expDatesNormal,columns=["Unix Date"])
+#         df = pd.read_json(urlData)
+    except:
+        print("Couldn't find", stock)
+        df = pd.DataFrame({'symbol':[stock],'shortName':['n.a.']})
+        df.set_index('symbol', inplace=True)
+        return df
+
+
+    try: 
+        df.set_index('symbol', inplace=True)
+    except:
+        df = pd.DataFrame({'symbol':[stock],'shortName':['n.a.']})
+        df.set_index('symbol', inplace=True)
     return df
-
-def optionChain(ticker='SPY', date='2022-11-18', calls_puts = 'calls'):
-    """
-    Scrapes option chain from Yahoo Fiance by converting date to unix code:
-        https://query2.finance.yahoo.com/v7/finance/options/{ticker}?date={date}
-        e.g.: https://query2.finance.yahoo.com/v7/finance/options/TSLA?date=1668729600
-    
-    Parameters
-    ----------
-    ticker : str, optional
-        DESCRIPTION. The default is 'SPY'.
-    date : str, optional
-        DESCRIPTION. Maturity date of options. Enter in yyy-mm-dd format. The default is '2022-11-18'.
-    calls_puts : str, optional
-        DESCRIPTION. Enter 'calls' or 'puts'. The default is 'calls'.
-    Returns
-    -------
-    dfOptions : DataFrame
-        Returns calls or puts option chain.
-    """
-    url = "https://query2.finance.yahoo.com/v7/finance/options/{}?date={}"
-    unixTS = pd.Timestamp('{} 00:00:00'.format(date)).timestamp()
-    url = url.format(ticker, int(unixTS))
-    data = pd.read_json(url)
-    optionsData = data['optionChain']['result'][0]['options'][0]
-    dfOptions = pd.DataFrame(optionsData[calls_puts])
-    dfOptions['Exp'] = pd.to_datetime(dfOptions['expiration'],origin="unix",unit='s')
-    return dfOptions
