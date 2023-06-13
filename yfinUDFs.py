e2d5a0de6fc1eb44eff6a33f0326147c35af057d
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2023-06-13 16:53:38 -0400

    Update yfinUDFs.py

--- yfinUDFs.py
+++ yfinUDFs.py
@@ -81,3 +81,46 @@ def fnYFinJSONAll(stock):
         df = pd.DataFrame({'symbol':[stock],'shortName':['n.a.']})
         df.set_index('symbol', inplace=True)
     return df
+
+
+#%% Option chains
+def grabExpDates(ticker):
+    """
+    Returns a dataframe of expiration dates available on Yahoo Finance.
+    e.g.: https://query2.finance.yahoo.com/v7/finance/options/SPY
+    
+    """
+    url = "https://query2.finance.yahoo.com/v7/finance/options/" + ticker
+    data = pd.read_json(url)
+    expDatesUnix = data['optionChain']['result'][0]['expirationDates']
+    expDatesNormal = pd.to_datetime(expDatesUnix, origin="unix",unit='s')
+    df = pd.DataFrame(expDatesUnix,index=expDatesNormal,columns=["Unix Date"])
+    return df
+
+def optionChain(ticker='SPY', date='2022-11-18', calls_puts = 'calls'):
+    """
+    Scrapes option chain from Yahoo Fiance by converting date to unix code:
+        https://query2.finance.yahoo.com/v7/finance/options/{ticker}?date={date}
+        e.g.: https://query2.finance.yahoo.com/v7/finance/options/TSLA?date=1668729600
+    
+    Parameters
+    ----------
+    ticker : str, optional
+        DESCRIPTION. The default is 'SPY'.
+    date : str, optional
+        DESCRIPTION. Maturity date of options. Enter in yyy-mm-dd format. The default is '2022-11-18'.
+    calls_puts : str, optional
+        DESCRIPTION. Enter 'calls' or 'puts'. The default is 'calls'.
+    Returns
+    -------
+    dfOptions : DataFrame
+        Returns calls or puts option chain.
+    """
+    url = "https://query2.finance.yahoo.com/v7/finance/options/{}?date={}"
+    unixTS = pd.Timestamp('{} 00:00:00'.format(date)).timestamp()
+    url = url.format(ticker, int(unixTS))
+    data = pd.read_json(url)
+    optionsData = data['optionChain']['result'][0]['options'][0]
+    dfOptions = pd.DataFrame(optionsData[calls_puts])
+    dfOptions['Exp'] = pd.to_datetime(dfOptions['expiration'],origin="unix",unit='s')
+    return dfOptions
