b1b8ef983701e7b3c9faa8e15040ce6952cd24c1
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2024-01-26 16:50:42 -0500

    Update 4_Options Calculator.py

--- pages/4_Options Calculator.py
+++ pages/4_Options Calculator.py
@@ -93,7 +93,7 @@ colInputs, colOutputs  = st.columns([1,1])
 with colInputs:
     ticker = st.text_input("Ticker:","SPY")
     expDF = yf.grabExpDates(ticker)
-    'link = "https://query2.finance.yahoo.com/v7/finance/options/{}?date=".format(ticker)
+    #link = "https://query2.finance.yahoo.com/v7/finance/options/{}?date=".format(ticker)
     link = "https://query2.finance.yahoo.com/v7/finance/options/{}?crumb=aQXXlwvY%2FHw".format(ticker)
     expDF['Link'] = expDF.apply(lambda x: link+str(x['Unix Date']), axis=1)
     allDates = list(expDF.index.strftime('%Y-%m-%d'))
