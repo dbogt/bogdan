46a807638e27d7c217f03b78bef5d456582ec17e
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2024-05-15 10:53:47 -0400

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+st.set_page_config(layout="wide",page_title='Bogdan Tudose',
+menu_items={
+        "About": f"Bogdan Python Demos"
+        f"\nApp contact: [Bogdan Tudose](mailto:bogdan.tudose@trainingthestreet.com)"
+    })
 st.sidebar.markdown("# Main page 🎈")
 
 text = """
