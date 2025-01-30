5687e288dac961e0895c2f44c47e01e0d24471a0
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2025-01-30 17:44:37 -0500

    ploty aquart

--- pages/8_Stats101.py
+++ pages/8_Stats101.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
