66c4e3644da6ea02c10a1bce825d9c31dccdc480
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2025-01-30 17:49:52 -0500

    aq

--- pages/8_Stats101.py
+++ pages/8_Stats101.py
@@ -156,7 +156,7 @@ fig_aq.update_layout(
     template="plotly_white",
     showlegend=False
 )
-st.write("The data in the 4 charts below all have the same variance, average, correlation, slope, and R2")
+st.write("The data in the 4 charts below all have the same variance, average, correlation, slope, and R2.")
 st.plotly_chart(fig_aq)
 st.write(stats)
 st.write(df)
