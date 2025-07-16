34c0c16564f0309544ff4d283c5054c84b9bb8fc
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2025-07-16 14:35:34 -0400

    Update 6_YFinUDFs.py

--- pages/6_YFinUDFs.py
+++ pages/6_YFinUDFs.py
@@ -55,7 +55,7 @@ def fnYFinJSONAll(stock):
 ticker = st.text_input("Enter a ticker")
 st.write("https://query1.finance.yahoo.com/v1/test/getcrumb")
 crumb = st.text_input("enter your crumb")
-st.write(crumbDynamic.text)
+#st.write(crumbDynamic.text)
 urlData = "https://query2.finance.yahoo.com/v7/finance/quote?symbols="+ticker
 st.write(urlData + "&crumb=" + crumb)
 params['crumb'] = st.secrets['crumb']
